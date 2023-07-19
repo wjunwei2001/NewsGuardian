d9dfc21e5130691611333fd31630af2683361dc0
Kuik Tze Cheng <tzecheng@Kuiks-MacBook-Pro.local>
2023-07-19 13:43:27 +0800

    updated frontend

--- frontend/src/NewsGuardian.js
+++ frontend/src/NewsGuardian.js
@@ -3,6 +3,17 @@ import React, { useState } from 'react';
 function NewsGuardian() {
     const [text, setText] = useState('')
     const [result,setResult] = useState('');
+    
+    const handleInputChange = (element) => { 
+      setText(element.target.value);
+
+      if (element) {
+      const target = element.target ? element.target : element; 
+      element.target.style.height = 'auto';
+      element.target.style.height = `${target.scrollHeight}px`;
+    }
+  };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         const response = await fetch('/predict/', {
@@ -17,11 +28,11 @@ function NewsGuardian() {
       };
 
     return (
-        <div>
-            <h1>NewsGuardian Fake News Detector</h1>
+        <div style={{ textAlign: 'center' }}>
+            <h1 style={{ textAlign: 'center' }} >NewsGuardian Fake News Detector</h1>
             <p style={{ textAlign: 'center' }}>Verify your information here</p>
             <form onSubmit={handleSubmit}>
-                <input type="text" id="userinput" name="text" placeholder="Enter your text here..." value={text} onChange={(e) => setText(e.target.value)} />
+                <textarea type="text" id="userinput" name="text" placeholder="Enter your text here..." value={text} onChange={handleInputChange} cols={60} />
                 <br />
                 <button type="submit" name="predict">Predict</button>
             </form>
