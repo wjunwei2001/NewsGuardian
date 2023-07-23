b2f7b28ccf548642dbf0cca00cc9c2290a879e0c
Wang Junwei <109643278+wjunwei2001@users.noreply.github.com>
2023-07-23 14:04:08 +0800

    Update application.py

--- backend/application.py
+++ backend/application.py
@@ -3,8 +3,9 @@ from flask_cors import CORS
 import nltk
 import re
 import pickle 
-custom_nltk_data_path = "backend/"
-nltk.data.path.append(custom_nltk_data_path)
+# custom_nltk_data_path = "backend/"
+# nltk.data.path.append(custom_nltk_data_path)
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from keras.models import load_model
